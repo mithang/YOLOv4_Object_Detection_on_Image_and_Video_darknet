f0fa34699ec92d77b2f39c249db2f1cd2a1df21a
Goldfish Developers <developer@goldfishcode.com>
2024-11-16 17:59:28 +0700

    output video image

---
+++ image_yolov3_out.sh
@@ -0,0 +1,6 @@
+
+
+./darknet detector test ./cfg/coco.data ./cfg/yolov3.cfg ./yolov3.weights data/dog.jpg -dont_show -out_filename dog_out.jpg
+
+
+
---
+++ video_yolov3_out.sh
@@ -0,0 +1,6 @@
+
+
+./darknet detector demo ./cfg/coco.data ./cfg/yolov3.cfg ./yolov3.weights test50.mp4 -dont_show -out_filename test50_out.mp4
+
+
+
